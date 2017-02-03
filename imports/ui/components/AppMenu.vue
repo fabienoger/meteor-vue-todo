8546962979453b16208829d3b929d91f1d665cbd
fabienoger <fab-oger@live.fr>
2017-02-03 11:17:03 +0100

    Add style to menu

--- imports/ui/components/AppMenu.vue
+++ imports/ui/components/AppMenu.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="app-menu">
-    <router-link :to="{ name:'home' }">Home</router-link>
-    <router-link :to="{ name:'tasks' }">Tasks</router-link>
+    <router-link :to="{ name:'home' }" class="menu-link">Home</router-link>
+    <router-link :to="{ name:'tasks' }" class="menu-link">Tasks</router-link>
   </div>
 </template>
+
+<style lang="scss">
+@import "~imports/ui/colors.scss";
+div.app-menu {
+  background-color: $primary-color;
+  padding: 0 10px;
+  height: 50px;
+  .menu-link {
+    display: inline-block;
+    color: #FFF;
+    text-decoration: none;
+    height: 50px;
+    line-height: 50px;
+    padding: 0 10px;
+    &:hover {
+      color: $primary-color;
+      background-color: #FFF;
+      border-bottom: 2px solid $primary-color;
+    }
+  }
+}
+</style>
